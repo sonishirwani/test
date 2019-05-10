6aaca3a2eb064139e689e5d596900ef481069289
soni <soni.shirwani@1eq.com>
2019-05-10 15:29:33 +0530

    This is my first commit

---
+++ src/test/java/TestCases/SeleniumTest.java
@@ -0,0 +1,18 @@
+package TestCases;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void demoTest1()
+	{
+		System.out.println("I am inside DemoTest1");
+	}
+	@Test
+	public void demoTest2()
+	{
+		System.out.println("I am inside DemoTest2");
+	}
+
+}
